c4bf06bfd2464002f5c3f38e36e657432f99e2e3
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 00:09:42 +0300

    Add web initializer

---
+++ hw_42_43/src/main/java/ru/itis/hw42/config/AppInitializer.java
@@ -0,0 +1,21 @@
+package ru.itis.hw42.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{DatabaseConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
