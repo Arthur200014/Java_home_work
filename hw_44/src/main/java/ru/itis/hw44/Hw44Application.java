a4b94048ac07ed721f4a1c275f777cf35b33a3f5
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 01:21:34 +0300

    Добавить точку входа Spring Boot

---
+++ hw_44/src/main/java/ru/itis/hw44/Hw44Application.java
@@ -0,0 +1,11 @@
+package ru.itis.hw44;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw44Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Hw44Application.class, args);
+    }
+}
