637890b3fa43645f139c94dd8322ae87d0013a4c
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:13:29 +0300

    user valid

---
+++ hw_48/src/main/java/ru/itis/hw48/user/NewUserRequest.java
@@ -0,0 +1,76 @@
+package ru.itis.hw48.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class NewUserRequest {
+
+    @NotBlank(message = "Имя не должно быть пустым")
+    private String firstName;
+
+    @NotBlank(message = "Фамилия не должна быть пустой")
+    private String lastName;
+
+    @NotBlank(message = "Email обязателен")
+    @Email(message = "Некорректный email")
+    private String email;
+
+    @NotBlank(message = "Пароль обязателен")
+    @Size(min = 6, message = "Пароль должен содержать минимум 6 символов")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$",
+            message = "Пароль должен содержать строчную, заглавную букву и цифру"
+    )
+    private String password;
+
+    @Min(value = 0, message = "Возраст не может быть меньше 0")
+    @Max(value = 120, message = "Возраст не может быть больше 120")
+    private Integer age;
+
+    public NewUserRequest() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
