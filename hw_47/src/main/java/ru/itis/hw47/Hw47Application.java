26f3cfd13cd273ccc46c3c72045d119ce5cba980
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:01:46 +0300

    main

---
+++ hw_47/src/main/java/ru/itis/hw47/Hw47Application.java
@@ -0,0 +1,12 @@
+package ru.itis.hw47;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw47Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw47Application.class, args);
+    }
+}
