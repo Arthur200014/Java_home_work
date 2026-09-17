4dfb4d16bee1b8e1485fad26e10709792895a9ff
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 01:51:18 +0300

    main class

---
+++ hw_46/src/main/java/ru/itis/hw46/Hw46Application.java
@@ -0,0 +1,12 @@
+package ru.itis.hw46;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw46Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw46Application.class, args);
+    }
+}
