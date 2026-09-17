6680501a416218b20950d4fc49d94438635d9495
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 01:03:17 +0300

    Fix database property resolution

--- hw_42_43/src/main/java/ru/itis/hw42/config/DatabaseConfig.java
+++ hw_42_43/src/main/java/ru/itis/hw42/config/DatabaseConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -38,6 +39,11 @@ public class DatabaseConfig {
 
     @Value("${db.driver}")
     private String driver;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public DataSource dataSource() {
