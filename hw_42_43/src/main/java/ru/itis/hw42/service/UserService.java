f4ffea3086a173d3eec37799c9005eba69b59e09
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 00:10:19 +0300

    Add user service

---
+++ hw_42_43/src/main/java/ru/itis/hw42/service/UserService.java
@@ -0,0 +1,37 @@
+package ru.itis.hw42.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
+import ru.itis.hw42.model.User;
+import ru.itis.hw42.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+    }
+
+    @Transactional
+    public User create(User user) {
+        user.setId(null);
+        return userRepository.save(user);
+    }
+}
