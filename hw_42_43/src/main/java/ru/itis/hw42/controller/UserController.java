9bb4c8770f88db76c524fd9c441f4adac3501896
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 00:10:24 +0300

    Add user controller

---
+++ hw_42_43/src/main/java/ru/itis/hw42/controller/UserController.java
@@ -0,0 +1,41 @@
+package ru.itis.hw42.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itis.hw42.model.User;
+import ru.itis.hw42.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User addUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+}
