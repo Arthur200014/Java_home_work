aa34262f623b883ebe7318edcd3482b7934980c3
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 00:10:00 +0300

    Add MVC configuration

---
+++ hw_42_43/src/main/java/ru/itis/hw42/config/WebConfig.java
@@ -0,0 +1,11 @@
+package ru.itis.hw42.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "ru.itis.hw42.controller")
+public class WebConfig {
+}
