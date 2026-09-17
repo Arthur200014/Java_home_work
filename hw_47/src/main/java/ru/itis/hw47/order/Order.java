b41b1240322da27e176b24eaead16ce212e3eaad
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:02:02 +0300

    add order

---
+++ hw_47/src/main/java/ru/itis/hw47/order/Order.java
@@ -0,0 +1,50 @@
+package ru.itis.hw47.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import ru.itis.hw47.account.Account;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate orderDate;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
