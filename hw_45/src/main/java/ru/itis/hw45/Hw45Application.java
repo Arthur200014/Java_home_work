76206bc896fd5da2aba19ddd9e3f8b523e14c4e2
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 01:30:18 +0300

    Add application class

---
+++ hw_45/src/main/java/ru/itis/hw45/Hw45Application.java
@@ -0,0 +1,12 @@
+package ru.itis.hw45;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw45Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw45Application.class, args);
+    }
+}
