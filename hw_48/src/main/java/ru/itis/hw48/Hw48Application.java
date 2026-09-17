c83bd3f934371721efaeadc9e0093137cc28d860
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:13:25 +0300

    main

---
+++ hw_48/src/main/java/ru/itis/hw48/Hw48Application.java
@@ -0,0 +1,12 @@
+package ru.itis.hw48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw48Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw48Application.class, args);
+    }
+}
