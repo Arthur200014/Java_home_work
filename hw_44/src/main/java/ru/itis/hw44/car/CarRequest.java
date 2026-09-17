bd51030af71ba0541bef2cd8a7476b65cb8a7aad
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 01:21:51 +0300

    Добавить DTO для создания автомобиля

---
+++ hw_44/src/main/java/ru/itis/hw44/car/CarRequest.java
@@ -0,0 +1,40 @@
+package ru.itis.hw44.car;
+
+public class CarRequest {
+    private String brand;
+    private String model;
+    private Integer year;
+    private Integer price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
