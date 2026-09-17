6b2f2ecb3744c36b99b686033b42ce381cdeb443
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:02:00 +0300

    acc controller

---
+++ hw_47/src/main/java/ru/itis/hw47/account/AccountController.java
@@ -0,0 +1,43 @@
+package ru.itis.hw47.account;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @Operation(summary = "Список аккаунтов")
+    @GetMapping
+    public List<AccountDto> getAll() {
+        return accountService.getAll();
+    }
+
+    @Operation(summary = "Аккаунт по id")
+    @GetMapping("/{id}")
+    public AccountDto getById(@PathVariable Long id) {
+        return accountService.getById(id);
+    }
+
+    @Operation(summary = "Создать аккаунт")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public AccountDto create(@RequestBody AccountRequest request) {
+        return accountService.create(request);
+    }
+}
