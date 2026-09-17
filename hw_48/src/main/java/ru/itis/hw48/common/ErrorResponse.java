e732d36f40e2b58b81bd89bfdf9d76802e845a01
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:13:56 +0300

    error resp

---
+++ hw_48/src/main/java/ru/itis/hw48/common/ErrorResponse.java
@@ -0,0 +1,22 @@
+package ru.itis.hw48.common;
+
+import java.time.LocalDateTime;
+
+public class ErrorResponse {
+
+    private final String message;
+    private final LocalDateTime timestamp;
+
+    public ErrorResponse(String message, LocalDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
