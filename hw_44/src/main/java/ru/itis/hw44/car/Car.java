7add0ec09276ac66157f7f3fbedbd0303c2cbdea
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 01:21:40 +0300

    Добавить сущность Car

---
+++ hw_44/src/main/java/ru/itis/hw44/car/Car.java
@@ -0,0 +1,57 @@
+package ru.itis.hw44.car;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+    private String model;
+    private Integer year;
+    private Integer price;
+
+    public Car() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
