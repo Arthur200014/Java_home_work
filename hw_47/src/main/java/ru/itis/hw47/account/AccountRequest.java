4d82c203c70c0f4df4ac667c58666aaf0e9a36a5
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 02:01:50 +0300

    acc dto

---
+++ hw_47/src/main/java/ru/itis/hw47/account/AccountRequest.java
@@ -0,0 +1,26 @@
+package ru.itis.hw47.account;
+
+public class AccountRequest {
+
+    private String name;
+    private String email;
+
+    public AccountRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
