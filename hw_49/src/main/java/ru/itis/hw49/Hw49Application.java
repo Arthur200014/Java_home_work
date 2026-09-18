cb104de3f08f5372b6570aa1426ec65c5f9b0012
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 11:48:09 +0300

    main

---
+++ hw_49/src/main/java/ru/itis/hw49/Hw49Application.java
@@ -0,0 +1,12 @@
+package ru.itis.hw49;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw49Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw49Application.class, args);
+    }
+}
