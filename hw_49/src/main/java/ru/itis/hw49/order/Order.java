dff37a15f4bbce423034420aa88460aa0a247d89
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 11:48:35 +0300

    order entity

---
+++ hw_49/src/main/java/ru/itis/hw49/order/Order.java
@@ -0,0 +1,42 @@
+package ru.itis.hw49.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import ru.itis.hw49.user.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime orderDate;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Order() {
+    }
+
+    public Order(Long id, LocalDateTime orderDate, User user) {
+        this.id = id;
+        this.orderDate = orderDate;
+        this.user = user;
+    }
+
+    public Long getId() { return id; }
+    public LocalDateTime getOrderDate() { return orderDate; }
+    public void setOrderDate(LocalDateTime orderDate) { this.orderDate = orderDate; }
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
+}
