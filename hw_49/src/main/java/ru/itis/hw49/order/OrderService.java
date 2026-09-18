2ed7eb13148047154fc707eac5bc4cf7d055a2f6
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 11:48:43 +0300

    order service

---
+++ hw_49/src/main/java/ru/itis/hw49/order/OrderService.java
@@ -0,0 +1,37 @@
+package ru.itis.hw49.order;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.itis.hw49.common.NotFoundException;
+import ru.itis.hw49.user.User;
+import ru.itis.hw49.user.UserService;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final UserService userService;
+
+    public OrderService(OrderRepository orderRepository, UserService userService) {
+        this.orderRepository = orderRepository;
+        this.userService = userService;
+    }
+
+    @Transactional
+    public OrderDto create(NewOrderRequest request) {
+        User user = userService.getEntity(request.getUserId());
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setOrderDate(request.getOrderDate());
+
+        return OrderDto.from(orderRepository.save(order));
+    }
+
+    public OrderDto getById(Long id) {
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Заказ " + id + " не найден"));
+
+        return OrderDto.from(order);
+    }
+}
