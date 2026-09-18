944f35efaccdf89e316ecae5f720287ba2aca0a4
Arthur200014 <103813672+Arthur200014@users.noreply.github.com>
2026-09-18 11:48:46 +0300

    order controller

---
+++ hw_49/src/main/java/ru/itis/hw49/order/OrderController.java
@@ -0,0 +1,33 @@
+package ru.itis.hw49.order;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderDto create(@Valid @RequestBody NewOrderRequest request) {
+        return orderService.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public OrderDto getById(@PathVariable Long id) {
+        return orderService.getById(id);
+    }
+}
